fe9bb6c01a5b2d13fb1cab7a70cd6bb896de9363
charredlot <charlotte.sp.tan@gmail.com>
2015-01-23 01:36:36 -0800

    add compass, fix disappearing issue

---
+++ Assets/scripts/Compass2D.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+@HideInInspector
+var target : GameObject;
+
+function Start () {
+	this.target = GameObject.Find("basic_document_r");
+}
+
+function Update () {
+	var x : float = this.target.transform.position.x - this.transform.position.x;
+	var y : float = this.target.transform.position.y - this.transform.position.y;
+	
+	var angle : float = Mathf.Atan(x / y) * Mathf.Rad2Deg;
+	
+	// handedness is backwards for some reason, probably investigate later	
+	this.transform.eulerAngles = new Vector3(0, 0, -angle);
+}
