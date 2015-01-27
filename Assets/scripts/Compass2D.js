7fe303652db1507af60b6a5b7068bf7f65f52511
charredlot <charlotte.sp.tan@gmail.com>
2015-01-27 01:59:06 -0800

    compass wasn't handling all quadrants correctly

--- Assets/scripts/Compass2D.js
+++ Assets/scripts/Compass2D.js
@@ -10,9 +10,23 @@ function Start () {
 function Update () {
 	var x : float = this.target.transform.position.x - this.transform.position.x;
 	var y : float = this.target.transform.position.y - this.transform.position.y;
+	var ax : float = Mathf.Abs(x);
+	var ay : float = Mathf.Abs(y);
 	
-	var angle : float = Mathf.Atan(x / y) * Mathf.Rad2Deg;
+	var angle : float = Mathf.Atan(ax / ay) * Mathf.Rad2Deg;
 	
-	// handedness is backwards for some reason, probably investigate later	
-	this.transform.eulerAngles = new Vector3(0, 0, -angle);
+	/* angle is always off y-axis in quadrant I since arrow starts pointing up */
+	if ((x > 0) && (y > 0)) {
+		/* Quadrant I */
+		this.transform.eulerAngles = new Vector3(0, 0, -angle);		
+	} else if ((x < 0) && (y > 0)) {
+		/* Quadrant II */
+		this.transform.eulerAngles = new Vector3(0, 0, angle);
+	} else if ((x < 0) && (y < 0)) {
+		/* Quadrant III */
+		this.transform.eulerAngles = new Vector3(0, 0, 180 - angle);
+	} else {
+		/* Quadrant IV */
+		this.transform.eulerAngles = new Vector3(0, 0, 180 + angle);
+	}	
 }
